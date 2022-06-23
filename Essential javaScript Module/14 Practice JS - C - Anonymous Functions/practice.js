509798b2020659537daa7deab1a38994b51bc6a8
Tiago Araujo <tiago.tsa1@hotmail.com>
2022-06-22 23:32:32 -0700

    add folders
    add anonumous functions exercise and Arrow functions exercise.

---
+++ Essential javaScript Module/14 Practice JS - C - Anonymous Functions/practice.js
@@ -0,0 +1,7 @@
+let doubleSpeed = function(velocity){
+    return velocity * 2
+}
+
+let newVelocity = doubleSpeed(50)
+
+alert(newVelocity)
---
+++ Essential javaScript Module/15 Practice JS - C - Arrow Functions/practice.js
@@ -0,0 +1,19 @@
+//
+
+const doubleVelocity = (velocity) => {
+    return velocity * 2
+}
+
+alert(doubleVelocity(50))
+
+//When there's only one line of code.
+
+const tripleVelocity = (velocity) => velocity * 3
+
+alert(tripleVelocity(50))
+
+//When it has only one parameter, we don't need "(parameter)"
+
+const quadrupleVelocity = velocity => velocity * 4
+
+alert(quadrupleVelocity(50))
